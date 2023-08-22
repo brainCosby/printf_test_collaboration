54cf3d00502f0b4cafc417ff26f283afb537af68
Fodi-Daniel-Nyabade <fodidaniel567@gmail.com>
2023-08-22 10:57:09 +0000

    updates

--- output_generator.c
+++ output_generator.c
@@ -9,8 +9,8 @@ void print_char(char c, int *char_count)
 
 void print_string(const char *str, int *char_count)
 {
-	_puts(*str);
-	(*char_count) += strlen(str);
+	_puts(str);
+	(*char_count) += _strlen(str);
 }
 
 void print_percent(int *char_count)
