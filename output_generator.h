299e2d211a11299c3e13f62610ee4e17e09a5dfe
Fodi-Daniel-Nyabade <fodidaniel567@gmail.com>
2023-08-22 04:21:37 +0000

    output generator header file

---
+++ output_generator.h
@@ -0,0 +1,8 @@
+#ifndef OUTPUT_GENERATOR_H_
+#define OUTPUT_GENERATOR_H_
+
+int _putchar(char c, int *char_count);
+void print_percent(int *char_count);
+int puts(const char *str, int *char_count);
+
+#endif
