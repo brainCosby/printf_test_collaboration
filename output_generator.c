df3be2ab8c4dabc24c27f2eaff7b7384f7e5173d
Fodi-Daniel-Nyabade <fodidaniel567@gmail.com>
2023-08-22 04:20:29 +0000

    output generator

---
+++ output_generator.c
@@ -0,0 +1,21 @@
+#include "output_generator.h"
+#include "printers.h"
+
+void print_char(char c, int *char_count)
+{
+	_putchar(c);
+	(*char_count)++;
+}
+
+void print_string(const char *str, int *char_count)
+{
+	_puts(*str);
+	(*char_count) += strlen(str);
+}
+
+void print_percent(int *char_count)
+{
+	_putchar('%');
+	(*char_count)++;
+
+}
