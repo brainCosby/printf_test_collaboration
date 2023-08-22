8064a0af8b40a27774765c72cf59b71e3ccfb3a9
Fodi-Daniel-Nyabade <fodidaniel567@gmail.com>
2023-08-22 11:28:43 +0000

    output generator header file update

--- output_generator.h
+++ output_generator.h
@@ -1,8 +1,8 @@
 #ifndef OUTPUT_GENERATOR_H_
 #define OUTPUT_GENERATOR_H_
 
-int _putchar(char c, int *char_count);
+void print_char(char c, int *char_count);
 void print_percent(int *char_count);
-int _puts(const char *str, int *char_count);
+void print_string(const char *str, int *char_count);
 
 #endif
